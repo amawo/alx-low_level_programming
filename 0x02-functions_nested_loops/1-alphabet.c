99c261a609ef839cf0d8e1251bd290508214fe4a
amawo <amatoullah.belarbi@gmail.com>
2023-04-25 15:12:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'a'; x++)
+		_putchar(x);
+	_putchar('\n');
+}
