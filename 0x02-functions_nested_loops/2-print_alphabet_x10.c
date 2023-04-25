2930e6f44545a7f2d267bf52981f628502a89560
amawo <amatoullah.belarbi@gmail.com>
2023-04-25 15:48:39 -0700

    2-main.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lower case
+ * repeat 10 times followed by new line
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		_putchar('\n');
+		i++;
+	}
+}
