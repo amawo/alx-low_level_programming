b9371afc24908630196d63f042853e1e17822ce1
amawo <amatoullah.belarbi@gmail.com>
2023-04-28 05:35:18 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - indicate if it is uppercase
+ * @c: integer to verifie
+ * return 0 or 1
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
