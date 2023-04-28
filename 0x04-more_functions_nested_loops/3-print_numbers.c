db1a87657077c97d8e00e406d5145c1e32d96094
amawo <amatoullah.belarbi@gmail.com>
2023-04-28 17:49:26 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9 
+ * followed by new line
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar("%d",i);
+	}
+	_putchar("\n");
+}
