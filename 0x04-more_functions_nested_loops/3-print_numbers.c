455181100004a4a293c7607f26c9a929a198cf3d
amawo <amatoullah.belarbi@gmail.com>
2023-04-28 17:52:28 +0100

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,11 @@
 
 void print_numbers(void)
 {
-	int i;
+	char i;
 
-	i = 0;
-	while (i <= 9)
+	for (i = '0';i <= '9';i++);
 	{
-		_putchar("%d",i);
+		_putchar(i);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
