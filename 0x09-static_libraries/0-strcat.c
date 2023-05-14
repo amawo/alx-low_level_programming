e506449a3b77f7b7f46ebbf87808ec1f53f30051
amawo <amatoullah.belarbi@gmail.com>
2023-05-14 03:06:57 -0700

    task0

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,1 +1,33 @@
-char *_strcat(char *dest, char *src);
+#include "main.h"
+
+ /**
+ * _strcat - concatenates two strings
+ * @dest: first input value
+ * @src: sec input value
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+
+	dest[i] = '\0';
+	return (dest);
+}
