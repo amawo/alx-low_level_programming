1203dfc1725bc42bdbf8c881b74b69a6b9330bf7
amawo <amatoullah.belarbi@gmail.com>
2023-05-22 14:16:12 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a struct named dog
+ * @name: element one
+ * @age: element two
+ * @owner: element tree
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
